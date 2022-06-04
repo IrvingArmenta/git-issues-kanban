1c63481cca3fc90f0ce13984a6ce115a22906270
IrvingArmenta <irvingarmentajd@gmail.com>
2022-06-04 09:29:27 +0900

    initial commit - setup

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+import { graphql } from '@octokit/graphql';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    console.log(
+      process.env.REACT_APP_GIT_TOKEN,
+      typeof process.env.REACT_APP_GIT_TOKEN
+    );
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div>
+        <button type="button" className="btn btn-primary">
+          Primary
+        </button>
+        <button type="button" className="btn btn-secondary">
+          Secondary
+        </button>
+        <button type="button" className="btn btn-success">
+          Success
+        </button>
+        <button type="button" className="btn btn-danger">
+          Danger
+        </button>
+        <button type="button" className="btn btn-warning">
+          Warning
+        </button>
+        <button type="button" className="btn btn-info">
+          Info
+        </button>
+        <button type="button" className="btn btn-light">
+          Light
+        </button>
+        <button type="button" className="btn btn-dark">
+          Dark
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
